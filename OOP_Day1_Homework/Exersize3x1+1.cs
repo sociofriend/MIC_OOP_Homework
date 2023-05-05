c8eb1271e639d4f9d7da75ac6e071d79f30c836d
sociofriend <lilit_avdalyan@yahoo.com>
2023-05-05 19:02:58 +0400

    Timetable,VatCalculator, CardinalToOrdinal, Factorial, Fibonacci solved

---
+++ OOP_Day1_Homework/Exersize3x1+1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace OOP_Day1_Homework
+{
+	public class Exersize3x1_1
+	{
+       public int Formula(int x)
+        {
+            if (x == 1)
+                return x;
+
+            else
+            {
+                x = 3*x + 1;
+                Console.WriteLine(x);
+
+                if (x != 1)
+                    Secuence(x);
+                else
+                    Formula(x);
+            }
+            return 00;
+
+        }
+
+
+       public int Secuence(int n)
+        {
+            if (n % 2 != 0)
+            {
+                n /= 2;
+                Formula(n);
+            }
+
+
+            else
+                Formula(n);
+            return 00;
+        }
+
+
+    }
+}
+
---
+++ OOP_Day1_Homework/FibonacciWithRec.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace OOP_Day1_Homework
+{
+	public class FibonacciWithRec
+	{
+        //List<int> fibSec = new List<int>() {0,1};
+
+        //public int FibNumber(int n)
+        //{
+        //	for (int i=2; i<n; i++)
+        //	{
+        //		fibSec.Add(fibSec[i - 2] + fibSec[i - 1]);
+        //	}
+
+        //	return fibSec[n - 1];
+
+        //}
+
+        public int FibNumber(int n)
+        {
+            if (n <= 1)
+                return n;
+           
+            else
+            {       
+                return FibNumber(n - 2) + FibNumber(n - 1);
+            }
+        }
+
+    }
+}
+
