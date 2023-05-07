fafd6edeb867898f0e7904c57316e09e07b2b763
sociofriend <lilit_avdalyan@yahoo.com>
2023-05-08 00:02:49 +0400

    Print Checkmate board

---
+++ OOP_Day2_Homework/CheckmateBoard.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace OOP_Day2_Homework
+{
+	public class CheckmateBoard
+	{
+		public void PrintCheckmatBoard()
+		{
+			string[] firstLastRow = new string[] {" ",  "a", "b", "c", "d", "e", "f", "g", "h", " "};
+			int[] firstLastColoumn = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+			string[] lightDarkRow = new string[] {"@", "#", "@", "#", "@", "#", "@", "#"};
+			string[] darkLightRow = new string[] {"#", "@", "#", "@", "#", "@", "#", "@"};
+
+			Console.WriteLine(string.Join(" ",firstLastRow));
+
+			// light cell = @; dark cell = #
+
+			for(int i = 2; i<=9; i++)
+			{
+				if (i % 2 == 0)
+				{
+					Console.Write(firstLastColoumn[i-2] +" ");
+					Console.Write(string.Join(" ", lightDarkRow));
+					Console.Write(" " + firstLastColoumn[i-2]);
+				}
+
+				else
+				{
+                    Console.Write(firstLastColoumn[i-2] + " ");
+                    Console.Write(string.Join(" ", darkLightRow));
+                    Console.Write(" " + firstLastColoumn[i-2]);
+                }
+				Console.WriteLine();
+            }
+
+            Console.WriteLine(string.Join(" ", firstLastRow));
+        }
+    }
+}
+
---
+++ OOP_Day2_Homework/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP_Day2_Homework;
+class Program
+{
+    static void Main(string[] args)
+    {
+        CheckmateBoard objBoard = new CheckmateBoard();
+        objBoard.PrintCheckmatBoard();
+    }
+}
+
