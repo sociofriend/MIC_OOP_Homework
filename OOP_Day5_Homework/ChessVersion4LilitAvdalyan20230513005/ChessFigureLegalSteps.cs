7f17ae536764e35ae0a77bda9d0f5a5147e5113d
sociofriend <lilit_avdalyan@yahoo.com>
2023-05-13 12:44:41 +0400

    Last edit on May 13th 2023

---
+++ OOP_Day5_Homework/ChessVersion4LilitAvdalyan20230513005/ChessFigureLegalSteps.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ChessVersion4LilitAvdalyan20230513005
+{
+	public class ChessFigureLegalSteps
+	{
+		public ChessFigureLegalSteps()
+		{
+		}
+	}
+}
+
---
+++ OOP_Day5_Homework/ChessVersion4LilitAvdalyan20230513005/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ChessVersion4LilitAvdalyan20230513005;
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
+
